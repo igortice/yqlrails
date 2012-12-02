0c0accf19a81d814480484059e97359e749ac9fc
igor rocha <igotice@gmail.com>
2012-12-02 17:28:39 -0300

    teste rest cliente ok

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.9'
 
 gem 'sqlite3'
 gem 'pry-rails'
+gem "rest-client"
 
 
 # Gems used only for assets and not required
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    url = URI.escape('http://query.yahooapis.com/v1/public/yql?q=select * from geo.states where place="Brasil"&format=json')
+    @estados_brasil = JSON.parse(RestClient.get url)
   end
 end
